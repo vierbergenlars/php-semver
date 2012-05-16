415c75e05b17f6d95965c3ce56723549fb430bba
Lars Vierbergen <vierbergenlars@gmail.com>
2012-05-16 16:31:31 +0200

    Let SemVerException accept a version string as second parameter

--- src/vierbergenlars/SemVer/SemVerException.php
+++ src/vierbergenlars/SemVer/SemVerException.php
@@ -1,4 +1,25 @@
 <?php
+
 namespace vierbergenlars\SemVer;
-class SemVerException extends \Exception {}
-?>
+
+class SemVerException extends \Exception {
+
+    protected $version = NULL;
+    protected $message = NULL;
+
+    function __construct($message, $version = NULL) {
+        $this->version = $version;
+        $this->message = $message;
+        parent::__construct($message . ' [[' . $version . ']]');
+    }
+
+    function getMessage() {
+        return $this->message;
+    }
+
+    function getVersion() {
+        return $this->version;
+    }
+
+}
+
