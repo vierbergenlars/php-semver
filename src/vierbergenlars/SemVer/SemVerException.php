370c77e2dc8eafe21c02cc43dc2181e908ce3244
Lars Vierbergen <vierbergenlars@gmail.com>
2012-05-16 16:39:23 +0200

    Fixes #20

--- src/vierbergenlars/SemVer/SemVerException.php
+++ src/vierbergenlars/SemVer/SemVerException.php
@@ -5,16 +5,10 @@ namespace vierbergenlars\SemVer;
 class SemVerException extends \Exception {
 
     protected $version = NULL;
-    protected $message = NULL;
 
     function __construct($message, $version = NULL) {
         $this->version = $version;
-        $this->message = $message;
         parent::__construct($message . ' [[' . $version . ']]');
-    }
-
-    function getMessage() {
-        return $this->message;
     }
 
     function getVersion() {
